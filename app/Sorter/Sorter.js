3880bf4bf202973da8eb718a33fa8d31acfa1c8c
Kaaasper <kasperhoflund@gmail.com>
2023-08-01 01:27:45 +0200

    dynamic resizing

--- app/Sorter/Sorter.js
+++ app/Sorter/Sorter.js
@@ -3,33 +3,46 @@
 import React from 'react';
 import './Sorter.css';
 
-const NUMBER_OF_ARRAY_BARS = 300;
-
 export default function Sorter() {
     const [array, setArray] = React.useState([]);
+    const [width, setWidth] = React.useState(0)
+    const [height, setHeight] = React.useState(0)
 
     React.useEffect(() => {
-        updateArray()
-    }, [])
+        // component is mounted and window is available
+        handleWindowResize();
+        window.addEventListener('resize', handleWindowResize);
+        // unsubscribe from the event on component unmount
+        return () => window.removeEventListener('resize', handleWindowResize);
+    }, []);
 
-    function updateArray() {
-        setArray(generateRandomNumbers(NUMBER_OF_ARRAY_BARS, 5, 600));
+    function updateArray(width, height) {
+        setArray(generateRandomNumbers((width - 200) / 4, 5, height - 300));
+    }
+
+    function handleWindowResize() {
+        setWidth(window.innerWidth);
+        setHeight(window.innerHeight);
+        updateArray(window.innerWidth, window.innerHeight);
     }
 
     return (
         <div className="array-container">
             {array.map((value, index) =>
             (
-                <div className="array-bar" key={index}>
-                    {value}
-                </div>
+                <div
+                    className="array-bar"
+                    key={index}
+                    style={{ height: `${value}px` }}></div>
             ))}
 
-            <button onClick={() => updateArray()}>Generate New Array</button>
+            <button className='button' onClick={() => updateArray(width, height)}>Generate a New Array</button>
         </div>
 
 
     );
+
+
 
 }
 
